aecd7e84eff1ad8bf2405045a7df194a10e80a05
Ilakiya2004 <145018409+Ilakiya2004@users.noreply.github.com>
2026-01-19 23:00:45 +0530

    Add variables and print statements in hello class

---
+++ variables.java
@@ -0,0 +1,26 @@
+package java_program;
+
+public class hello {
+	
+	public static void main(String arg[]) {
+	
+
+	String name= "ilakiya";
+	int age= 20;
+	boolean isStudent= true	;
+	char grade= 'A';
+	double height= 5.4;
+	
+	System.out.println("Name: " +name);
+	System.out.println("Age:" +age);
+	System.out.println("IsStudent:" +isStudent);
+	System.out.println("Grade:" +grade);
+	System.out.println("Height:" +height);
+	}
+	}
+	
+
+
+
+
+
