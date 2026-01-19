36c50674b6a2dadee685f455c10f9c5b34d0bd8a
Ilakiya2004 <145018409+Ilakiya2004@users.noreply.github.com>
2026-01-19 23:03:46 +0530

    Add Operators_Ex class with right shift examples
    Implement bitwise right shift operations in Operators_Ex.

---
+++ operators.java
@@ -0,0 +1,26 @@
+package java_program;
+
+public class Operators_Ex {
+
+
+public static void main(String args[]) {
+    
+ int x = 7;
+		 int y = 8;
+		 System.out.println(x >> 2);
+		 System.out.println(y >> 5);
+		 System.out.println(x >> 9);
+		 System.out.println(y >> 9);
+		 
+ 
+			
+	
+			
+			
+			
+			
+			
+			
+			
+}
+}
