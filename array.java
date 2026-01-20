320f7fb5c5c40521bbed05de41e395e984756733
Ilakiya2004 <145018409+Ilakiya2004@users.noreply.github.com>
2026-01-20 22:55:01 +0530

    Create array.java to demonstrate array usage
    Implement a Java program that initializes and prints an array of integers.

---
+++ array.java
@@ -0,0 +1,41 @@
+package java_program;
+
+public class array {
+
+
+ public static void main(String args[]) {
+	
+	 int[] box = {10,20,30,40,50,60,70,80,90,100};
+	 
+	box[0] = 10;
+	box[1] = 20;
+	box[2] = 30;
+	box[3] = 40;
+	box[4] = 50;
+	box[5] = 60;
+	box[6] = 70;
+	box[7] = 80;
+	box[8] = 90;
+	box[9] = 100;
+	
+	System.out.println(box[0]);
+	System.out.println(box[1]);
+	System.out.println(box[2]);
+
+	System.out.println(box[3]);
+
+	System.out.println(box[4]);
+
+	System.out.println(box[5]);
+
+	System.out.println(box[6]);
+
+
+	System.out.println(box[7]);
+	System.out.println(box[8]);
+
+	System.out.println(box[9]);
+
+
+	
+ }}
